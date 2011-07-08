9ae17dc661e0d19def727b14a7a78770e36f0c03
Negreanu Marius Groleo <groleo@gmail.com>
2011-07-09 02:07:39 +0300

    add some vertical spacing

---
+++ test.sh
@@ -0,0 +1,24 @@
+#! /bin/bash -e
+
+
+SRCDIR=/home/mariusn/workspace/mine/
+WWWDIR=/home/mariusn/public_html/
+
+rm -rf $WWWDIR/*
+
+CGI_BIN=$WWWDIR/cgi-bin/
+mkdir $CGI_BIN
+
+. setup-env.sh $SRCDIR
+cd $SRCDIR
+$CC 1167.c
+cd -
+./dxr-index.py
+
+cp -r www/js $WWWDIR
+cp -r www/images/ $WWWDIR
+cp -r www/*.css $WWWDIR
+
+cp dxr.config $CGI_BIN
+cp www/search.cgi $CGI_BIN
+cp www/getinfo.cgi $CGI_BIN
